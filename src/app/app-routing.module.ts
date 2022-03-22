cfcaf309a7dcce66c40b063d3b367cc35fea6a5c
Ramón Molinero <molinero.gomez.ramon@gmail.com>
2022-03-22 13:43:02 +0100

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'ingresos-egresos', loadChildren: () => import('./ingreso-egreso/ingreso-egreso.module').then(m => m.IngresoEgresoModule)},
+  {path: '', component: DashboardComponent},
+  {path: '**', redirectTo: '', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
